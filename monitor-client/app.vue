2d4ee7614d80d8cf6c8788f21e4159840774614e
João Miguel <2221438@my.ipleiria.pt>
2024-11-15 16:41:48 +0000

    vue showing packages working

--- monitor-client/app.vue
+++ monitor-client/app.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div v-if="error">
+    Error: {{ error.message }}
+  </div>
+  <div v-else>
+    <h2>Packages</h2>
+    <div>{{ packages }}</div>
+    <button @click.prevent="refresh">Refresh Data</button>
   </div>
 </template>
+
+
+<script setup>
+const config = useRuntimeConfig();
+const api = config.public.API_URL;
+const { data: packages, error, refresh } = await useFetch(`${api}/packages/all`)
+</script>
